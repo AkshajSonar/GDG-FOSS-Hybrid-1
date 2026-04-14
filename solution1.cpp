8f77dd05d8dbd34db041cc2760d2b7e56c6d05ab
divit030 <divitpandey30@gmail.com>
2026-04-14 18:40:55 +0530

    Corrected the code

--- solution1.cpp
+++ solution1.cpp
@@ -6,14 +6,7 @@ int main() {
     while(t--){
         long long n, k;
         cin >> n >> k;
-        long long count = 0, current = 0;
-        while (count < k) {
-            current++;
-            if (current % n != 0) count++;
-        }
-
-        
-        cout << current << endl;
+        cout << k + (k - 1) / (n - 1) << endl;
     }
     return 0;
 }
